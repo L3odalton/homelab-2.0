c4b2447c55337a298e2c4e9e47198d9f7ab83c31
Linus Baumann <keen.key5715@linus-baumann.de>
2025-03-03 19:23:10 +0100

    add terraform code

---
+++ terraform/srv-github-runner/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_version = ">= 0.13.0"
+    required_providers {
+        proxmox = {
+            source = "Telmate/proxmox"
+            version = "3.0.1-rc6"
+        }
+    }
+}
+
+provider "proxmox" {
+    pm_api_url = var.proxmox_api_url
+    pm_api_token_id = var.proxmox_api_token_id
+    pm_api_token_secret = var.proxmox_api_token_secret
+}
