b5efc537e0584da352a79628af9f6db2bd00be64
Linus Baumann <keen.key5715@linus-baumann.de>
2025-05-17 11:16:00 +0200

    feat(terraform): add configuration for proxmox docker test server
    Add Terraform configuration files to provision a Proxmox VM for a Docker test server. Includes variables, provider, and machine resource definitions to set up the VM with specific network, disk, and cloud-init settings.

---
+++ terraform/proxmox/srv-docker-test/machine.tf
@@ -0,0 +1,54 @@
+resource "proxmox_vm_qemu" "srv-docker-test" {
+    name = "srv-docker-test"
+    target_node = "pve-s13-02"
+    vmid = "100239"
+    desc = "docker test"
+    bios = "ovmf"
+    onboot = true
+    vm_state = "running"
+    agent = 1
+    clone_id = "7002"
+    full_clone = true
+    scsihw = "virtio-scsi-pci"
+    cores = 2
+    sockets = 1
+    cpu_type = "host"
+    memory = 2048
+    automatic_reboot = true
+
+    os_type = "cloud-init"
+    cicustom = "vendor=local:snippets/qemu-guest-agent.yml"
+    ciuser = "mgrsys"
+    ciupgrade = true
+    searchdomain = "local.darksaber.fyi"
+    nameserver = "10.0.100.241,10.0.100.242"
+    ipconfig0 = "ip=10.0.100.10/24,gw=10.0.100.1"
+    sshkeys = <<EOF
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK43atgPI2XVBwvSM5hGhbeokxgKuetThnThHEJRMiAK
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuOhwlgxLF/4v06qha9jPQ4egOaW2nUFlC7988tPh9X
+    EOF
+
+    network {
+        id = 0
+        bridge = "vmbr0"
+        model  = "virtio"
+    }
+
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                    storage = "local-lvm"
+                    size    = "20G" 
+                }
+            }
+        }
+        ide {
+            ide1 {
+                cloudinit {
+                    storage = "local-lvm"
+                }
+            }
+        }
+    }
+}
