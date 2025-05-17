2674ba3685e0c438106cf4fd3c388e6bdb2e4236
Linus Baumann <keen.key5715@linus-baumann.de>
2025-05-17 12:37:17 +0200

    refactor(variables): rename Proxmox API variables for clarity

--- terraform/proxmox/srv-docker-test/variables.tf
+++ terraform/proxmox/srv-docker-test/variables.tf
@@ -1,13 +1,16 @@
-variable "proxmox_api_url" {
+variable "pm_api_url" {
     type = string
+    description = "The Proxmox API URL"
 }
 
-variable "proxmox_api_token_id" {
+variable "pm_user" {
     type = string
+    description = "The Proxmox API token ID"
     sensitive = true
 }
 
-variable "proxmox_api_token_secret" {
+variable "pm_pass" {
     type = string
+    description = "The Proxmox API token secret"
     sensitive = true
 }
