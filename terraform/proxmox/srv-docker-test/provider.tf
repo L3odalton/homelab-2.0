ca52df2bd4dbcc643a9a34a04fe5c30a2208627f
Linus Baumann <keen.key5715@linus-baumann.de>
2025-05-17 11:59:20 +0200

    ci: add terraform deployment workflow and update provider config
    Add a new GitHub Actions workflow for Terraform deployment to automate infrastructure changes. Also, update the Proxmox provider configuration to align with the new variable names used in the workflow.

--- terraform/proxmox/srv-docker-test/provider.tf
+++ terraform/proxmox/srv-docker-test/provider.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "proxmox" {
-    pm_api_url = var.proxmox_api_url
-    pm_api_token_id = var.proxmox_api_token_id
-    pm_api_token_secret = var.proxmox_api_token_secret
+    pm_api_url = var.pm_api_url
+    pm_api_token_id = var.pm_user
+    pm_api_token_secret = var.pm_pass
 }
