ea2bcab26f9d2f1a234b22281e4a7e0119ba43f9
Linus Baumann <keen.key5715@linus-baumann.de>
2025-05-18 17:07:34 +0200

    fix(proxmox): update IP address for srv-docker-test VM
    The IP address for the srv-docker-test VM was changed from 10.0.100.10 to 10.0.100.239 to avoid conflicts and align with the updated network configuration.

--- terraform/proxmox/srv-docker-test/machine.tf
+++ terraform/proxmox/srv-docker-test/machine.tf
@@ -19,7 +19,7 @@ resource "proxmox_vm_qemu" "srv-docker-test" {
     ciupgrade = true
     searchdomain = "local.darksaber.fyi"
     nameserver = "10.0.100.241,10.0.100.242"
-    ipconfig0 = "ip=10.0.100.10/24,gw=10.0.100.1"
+    ipconfig0 = "ip=10.0.100.239/24,gw=10.0.100.1"
     sshkeys = <<EOF
     ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK43atgPI2XVBwvSM5hGhbeokxgKuetThnThHEJRMiAK
     ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuOhwlgxLF/4v06qha9jPQ4egOaW2nUFlC7988tPh9X
