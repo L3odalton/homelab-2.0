5c529b4b8602195aa6f41674da8aa6fae416b9ce
L3odalton <keen.key5715@linus-baumann.de>
2025-06-22 19:55:48 +0200

    add srv-docker-prod resource

---
+++ terraform/proxmox/srv-docker-prod/machine.tf
@@ -0,0 +1,58 @@
+resource "proxmox_vm_qemu" "srv-docker-prod" {
+    name = "srv-srv-docker-prod"
+    target_node = "pve-ugreen-03"
+    vmid = "100101"
+    desc = "docker proxy"
+    bios = "ovmf"
+    onboot = true
+    vm_state = "running"
+    agent = 1
+    clone_id = "7003"
+    full_clone = true
+    scsihw = "virtio-scsi-pci"
+    memory = 8192
+    automatic_reboot = true
+
+    os_type = "cloud-init"
+    cicustom = "vendor=local:snippets/qemu-guest-agent.yml"
+    ciuser = "mgrsys"
+    ciupgrade = true
+    searchdomain = "local.darksaber.fyi"
+    nameserver = "10.0.100.241,10.0.100.242"
+    ipconfig0 = "ip=10.0.100.101/24,gw=10.0.100.1"
+    sshkeys = <<EOF
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK43atgPI2XVBwvSM5hGhbeokxgKuetThnThHEJRMiAK
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuOhwlgxLF/4v06qha9jPQ4egOaW2nUFlC7988tPh9X
+    EOF
+
+    cpu {
+        cores = 4
+        sockets = 1
+        type = "x86-64-v3"
+    }
+
+    network {
+        id = 0
+        bridge = "vmbr0"
+        model  = "virtio"
+    }
+
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                    storage = "local-zfs"
+                    size    = "100G"
+                    discard = "true"
+                }
+            }
+        }
+        ide {
+            ide1 {
+                cloudinit {
+                    storage = "local-zfs"
+                }
+            }
+        }
+    }
+}
